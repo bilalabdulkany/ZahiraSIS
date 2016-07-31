e833a74261e6058e615a6332717f1efb75802f72
bilalincorporated <bilal.abdulkany@gmail.com>
2016-08-01 00:06:45 +0530

    first

--- ZahiraSIS/Program.cs
+++ ZahiraSIS/Program.cs
@@ -16,7 +16,7 @@ namespace ZahiraSIS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
--- ZahiraSIS/Properties/Settings.Designer.cs
+++ ZahiraSIS/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZahiraSIS.Properties
-{
-
-
+namespace ZahiraSIS.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ABDULKANY\\SQLEXPRESS32;Initial Catalog=Zahira_SIS;Integrated Security" +
+            "=True")]
+        public string Zahira_SISConnectionString {
+            get {
+                return ((string)(this["Zahira_SISConnectionString"]));
             }
         }
     }
