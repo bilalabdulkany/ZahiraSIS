f5f3d92ca6e6bb68564c1865ec36a991835e995e
bilalabdulkany <merdocbial.89@gmail.com>
2016-08-03 23:16:25 +0530

    dashboard

---
+++ ZahiraSIS/Dashboard.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZahiraSIS
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Report = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStudentClassReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentDetailReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Student Class Report";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Report});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(433, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Report
+            // 
+            this.Report.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuStudentClassReport,
+            this.studentDetailReportToolStripMenuItem});
+            this.Report.Name = "Report";
+            this.Report.Size = new System.Drawing.Size(54, 20);
+            this.Report.Text = "Report";
+            this.Report.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // mnuStudentClassReport
+            // 
+            this.mnuStudentClassReport.Name = "mnuStudentClassReport";
+            this.mnuStudentClassReport.Size = new System.Drawing.Size(186, 22);
+            this.mnuStudentClassReport.Text = "Student Class Report";
+            this.mnuStudentClassReport.Click += new System.EventHandler(this.mnuStudentClassReport_Click);
+            // 
+            // studentDetailReportToolStripMenuItem
+            // 
+            this.studentDetailReportToolStripMenuItem.Name = "studentDetailReportToolStripMenuItem";
+            this.studentDetailReportToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.studentDetailReportToolStripMenuItem.Text = "Student Detail Report";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 274);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Report;
+        private System.Windows.Forms.ToolStripMenuItem mnuStudentClassReport;
+        private System.Windows.Forms.ToolStripMenuItem studentDetailReportToolStripMenuItem;
+    }
+}
---
+++ ZahiraSIS/Dashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZahiraSIS
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnuStudentClassReport_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
